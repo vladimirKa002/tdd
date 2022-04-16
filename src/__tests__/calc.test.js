467aebc7b95c2dd7b24caaba02f65018920611a5
grinikita <Grigoriev98>
2022-04-16 09:25:15 +0300

    2 wave

--- src/__tests__/calc.test.js
+++ src/__tests__/calc.test.js
@@ -4,5 +4,14 @@ describe('initial test', () => {
     it('do some simple stuff', () => {
         expect(stringCalc('')).toBe(0);
     });
-})
+});
 
+describe('second tests wave', () => {
+    it('adds numbers', () => {
+        expect(stringCalc('1')).toBe(1);
+    });
+
+    it('adds numbers', () => {
+        expect(stringCalc('1,2')).toBe(3);
+    });
+})
