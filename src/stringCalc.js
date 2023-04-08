ca070c07d7f187d6e91952c4e56d2e6d244f0650
vladimirKa002 <kvvgames18@gmail.com>
2023-04-08 11:37:08 +0300

    calc

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,3 +1,18 @@
-const calc = () => 0;
+const calc = (str) => {
+    var breakpoint = ""
+    if (str.startsWith("//")) {
+        const data = str.split("\n", 2)
+        str = data[1]
+        breakpoint = data[0].substring(2)
+    }
+    else breakpoint = /,|\n/
+    const splitted = str.split(breakpoint)
+    if (splitted == "") return 0
+    var result = 0
+    for (let i = 0; i < splitted.length; i++) {
+        result += parseInt(splitted[i])
+    }
+    return result
+};
 
 export default calc;
